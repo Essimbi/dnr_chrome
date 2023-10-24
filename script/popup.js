8cc6341fd395292806629ee0371a3bee6bf034fe
Essimbi <essimbideranot@gmail.com>
2023-10-24 17:00:10 +0100

    End of the first version

--- script/popup.js
+++ script/popup.js
@@ -2,7 +2,17 @@ document.getElementById("submit").addEventListener("click", async () => {
 
     const domain = document.getElementById("domain").value;
 
-    const history = await chrome.history.search({ text: domain });
+    // Début d'année
+    const start = new Date(new Date().getFullYear(), 0, 1).getTime();
+
+    // Fin d'année  
+    const end = new Date(new Date().getFullYear(), 11, 31).getTime();
+
+    const history = await chrome.history.search({
+        text: domain,
+        startTime: start,
+        endTime: end
+    });
 
     history.forEach(async item => {
         await chrome.history.deleteUrl({ url: item.url });
