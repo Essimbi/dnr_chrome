a4807882001bcc64762325b3b5fcf407fdb4785b
Essimbi <essimbideranot@gmail.com>
2023-10-24 08:40:18 +0100

    First end

--- script/popup.js
+++ script/popup.js
@@ -4,26 +4,10 @@ document.getElementById("submit").addEventListener("click", async () => {
 
     const history = await chrome.history.search({ text: domain });
 
-    history.forEach(item => {
-        chrome.history.deleteUrl({ url: item.url });
+    history.forEach(async item => {
+        await chrome.history.deleteUrl({ url: item.url });
     });
 
+    alert("Ok");
+
 });
-
-// Récupérer l'historique
-// chrome.history.search({
-//     text: '', // Filtrer par texte (optionnel)
-//     startTime: 0, // Début de la recherche (optionnel) 
-//     maxResults: 50 // Nombre max de résultats (optionnel)
-// }, function (historyItems) {
-
-//     // Afficher l'historique
-//     historyItems.forEach(function (historyItem) {
-
-//         console.log('URL: ' + historyItem.url);
-//         console.log('Titre: ' + historyItem.title);
-//         console.log('Date: ' + historyItem.lastVisitTime);
-
-//     });
-
-// });
