e296e4b1928c4dc091affa8a44114acb5d318c33
Essimbi <essimbideranot@gmail.com>
2023-10-24 17:13:35 +0100

    Control du champs de saisie

--- script/popup.js
+++ script/popup.js
@@ -1,23 +1,40 @@
+// Fonction de suppression des occurences des noms de domaines
 document.getElementById("submit").addEventListener("click", async () => {
 
+    // Récupération du nom de domaine provenant du formulaire
     const domain = document.getElementById("domain").value;
 
-    // Début d'année
-    const start = new Date(new Date().getFullYear(), 0, 1).getTime();
+    if (domain === '') {
+        alert("Champ de saisie vide !!");
+    }
+    else {
+        // Début d'année
+        const start = new Date(new Date().getFullYear(), 0, 1).getTime();
 
-    // Fin d'année  
-    const end = new Date(new Date().getFullYear(), 11, 31).getTime();
+        // Fin d'année  
+        const end = new Date(new Date().getFullYear(), 11, 31).getTime();
 
-    const history = await chrome.history.search({
-        text: domain,
-        startTime: start,
-        endTime: end
-    });
+        // Recherche du nom de domaine dans l'historique, 
+        // la recherche sur fait sur toute l'année courante d'où la présence des paramètres *startTime* et *endTime*
+        const history = await chrome.history.search({
+            text: domain,
+            startTime: start,
+            endTime: end
+        });
 
-    history.forEach(async item => {
-        await chrome.history.deleteUrl({ url: item.url });
-    });
+        if (history.length === 0) {
+            // afficher message d'erreur
+            alert("Aucune occurrence de ce domaine trouvée");
+        } else {
 
-    alert("Ok");
+            // Suppression de chaque occurence du nom de domaine
+            history.forEach(async item => {
+                await chrome.history.deleteUrl({ url: item.url });
+            });
+
+            alert("Suppression terminée");
+
+        }
+    }
 
 });
